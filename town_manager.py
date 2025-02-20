552e2069aeb42f27862cea1a67c3971aabde6a24
Beastman-dev <wmeastman@gmail.com>
2025-02-20 15:46:45 -0500

    Updated town manager and deleted pycache

--- town_manager.py
+++ town_manager.py
@@ -22,19 +22,17 @@ import npc_manager
 # Services: Buy, Sell, Trade
 # Patron: Merchant Tavros Goldleaf
 
-# Specific Town NPCs
+# Global variables
 Town_NPCs = {
-    "Innkeeper Davrid Havenrest": Civilian("Davrid Havenrest", "Innkeeper", 3, 4, 4, 1.5, 2),
-    "Blacksmith Gendry Ironheart": Civilian("Gendry Ironheart", "Blacksmith", 3, 2, 2, 1.5, 2),
-    "Alchemist Elara Moonshadow": Civilian("Elara Moonshadow", "Alchemist", 4, 3, 3, 1.5, 2),
-    "Matron Sylia Lightbringer": Civilian("Sylia Lightbringer", "Cleric", 4, 4, 4, 1.5, 2),
-    "Merchant Tavros Goldleaf": Civilian("Tavros Goldleaf", "Merchant", 3, 3, 3, 1.5, 2)
+    "Innkeeper Davrid Havenrest": npc_manager.Civilian("Davrid Havenrest", "Innkeeper", 3, 4, 4, 1.5, 2),
+    "Blacksmith Gendry Ironheart": npc_manager.Civilian("Gendry Ironheart", "Blacksmith", 3, 2, 2, 1.5, 2),
+    "Alchemist Elara Moonshadow": npc_manager.Civilian("Elara Moonshadow", "Alchemist", 4, 3, 3, 1.5, 2),
+    "Matron Sylia Lightbringer": npc_manager.Civilian("Sylia Lightbringer", "Cleric", 4, 4, 4, 1.5, 2),
+    "Merchant Tavros Goldleaf": npc_manager.Civilian("Tavros Goldleaf", "Merchant", 3, 3, 3, 1.5, 2)
 }
-
 
 misc_npc_names = ["Bedru", "Tucyr", "Ogden", "Gauer", "Fulcor", "Strath", "Hodun-zak", "Gendow", "Sorlon", "Dalvas", "Hap Lu", "Loran", "Arnesan", "Fruela"]
 
-# Town Locations
 Town_Locations = {
     "Inn": {
         "Name": "Lazy Dragon Inn and Tavern",
@@ -62,3 +60,11 @@ Town_Locations = {
         "Patron": Town_NPCs["Merchant Tavros Goldleaf"]
     }
 }
+
+def list_town_locations():
+    print("Town Locations:\n")
+    for location in Town_Locations:
+        print(f"  {location}: {Town_Locations[location]['Name']}")
+    return
+
+list_town_locations()
