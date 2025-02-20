98e4c75bbdd399a97a0f1f3d7ffcac193f580827
Beastman-dev <wmeastman@gmail.com>
2025-02-20 13:58:37 -0500

    Added NPC unit test and npc set for testing other systems

--- npc_manager.py
+++ npc_manager.py
@@ -1,6 +1,8 @@
 # For generating and managing NPCs, including enemies and allies.
 
-class Enemy:
+import os
+
+class Combatant:
     def __init__(self, name, type, challenge_rating, max_hp, max_mana, physical, mental, social, combat_skill, magic_skill, charm_skill, weapon_modifier, spell_modifier, social_modifier, physical_resistance, magic_resistance, charm_resistance, exp):
         self.name = name
         self.type = type
@@ -38,8 +40,69 @@ class Enemy:
         social_defense = self.social + self.charm_resistance
         return social_attack, social_defense
 
+class Civilian:
+    def __init__(self, name, type, mental, social, charm_skill, social_modifier, charm_resistance):
+        self.name = name
+        self.type = type
+        self.mental = mental
+        self.social = social
+        self.charm_skill = charm_skill
+        self.social_modifier = social_modifier
+        self.charm_resistance = charm_resistance
 
-enemy_creature = Enemy("Goblin", "Humanoid", 1, 50, 50, 2, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 100)
-print(f"Enemy Attack: {enemy_creature.combat_power()[0]} Enemy Defense: {enemy_creature.combat_power()[1]}")
-print(f"Enemy Magic Attack: {enemy_creature.magic_power()[0]} Enemy Magic Defense: {enemy_creature.magic_power()[1]}")
-print(f"Enemy Social Attack: {enemy_creature.social_power()[0]} Enemy Social Defense: {enemy_creature.social_power()[1]}")
+    def social_power(self):
+        social_attack = self.social + self.charm_skill * self.social_modifier
+        social_defense = self.social + self.charm_resistance
+        return social_attack, social_defense
+    
+class NPCManager:
+    def __init__(self):
+        self.npcs = []
+        self.civilians = []
+        self.enemies = []
+        self.allies = []
+
+    def add_npc(self, npc):
+        self.npcs.append(npc)
+        if npc.type == "Enemy":
+            self.enemies.append(npc)
+        elif npc.type == "Ally":
+            self.allies.append(npc)
+        elif npc.type == "Civilian":
+            self.civilians.append(npc)
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+    return
+
+def test_npcs():
+    npc_manager = NPCManager()
+    goblin = Combatant("Goblin", "Enemy", 1, 50, 50, 2, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 100)
+    npc_manager.add_npc(goblin)
+    hired_sword = Combatant("Hired Sword", "Ally", 1, 50, 50, 2, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 100)
+    npc_manager.add_npc(hired_sword)
+    blacksmith = Civilian("Blacksmith", "Civilian", 1, 1, 1, 1, 1)
+    npc_manager.add_npc(blacksmith)
+
+def unit_test():
+    npc_manager = NPCManager()
+    goblin = Combatant("Goblin", "Enemy", 1, 50, 50, 2, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 100)
+    npc_manager.add_npc(goblin)
+    clear_screen()
+    print("NPC Manager Test")
+    print(f"\n  NPC: {npc_manager.enemies[0].name}")
+    print(f"  Attack/Defense: {npc_manager.enemies[0].combat_power()}")
+    print(f"  Magic A/D: {npc_manager.enemies[0].magic_power()}")
+    print(f"  Social A/D: {npc_manager.enemies[0].social_power()}")
+    blacksmith = Civilian("Blacksmith", "Civilian", 1, 1, 1, 1, 1)
+    npc_manager.add_npc(blacksmith)
+    print(f"\n  NPC: {npc_manager.civilians[0].name}")
+    print(f"  Social A/D: {npc_manager.civilians[0].social_power()}")
+    hired_sword = Combatant("Hired Sword", "Ally", 1, 50, 50, 2, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 100)
+    npc_manager.add_npc(hired_sword)
+    print(f"\n  NPC: {npc_manager.allies[0].name}")
+    print(f"  Combat Attack/Defense: {npc_manager.allies[0].combat_power()}")
+    print(f"  Magic A/D: {npc_manager.allies[0].magic_power()}")
+    print(f"  Social A/D: {npc_manager.allies[0].social_power()}")
+
+#unit_test()
